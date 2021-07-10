fd28fcf5f1a294af1a22bc37d84b88d1e6d32303
Jordi Vilaplana <jordivilaplanamarti@gmail.com>
2021-07-10 20:16:33 +0200

    Channels pin reference refactored into struct types

--- two-channel-rgb-controller/arduino-code/arduino-code.ino
+++ two-channel-rgb-controller/arduino-code/arduino-code.ino
@@ -17,26 +17,29 @@
 
 #include <math.h>
 
-// RGB sub-channel identifiers
-int const RED = 0;
-int const GREEN = 1;
-int const BLUE = 2;
+// Red-Green-Blue data struct
+// This can be used for both RGB values and I/O pin reference
+struct Channel {
+  int pin_red;
+  int pin_green;
+  int pin_blue;
+};
 
 // RGB potentiometer input pins {R, G, B}
-int const IN_1_PIN[] = {A7, A6, A5};
-int const IN_2_PIN[] = {A4, A3, A2};
+Channel const IN_1 = {A7, A6, A5};
+Channel const IN_2 = {A4, A3, A2};
 
 // RGB PWM output pins {R, G, B}
-int const OUT_1_PIN[] = {6, 5, 3};
-int const OUT_2_PIN[] = {11, 10, 9};
+Channel const OUT_1 = {6, 5, 3};
+Channel const OUT_2 = {11, 10, 9};
 
-// Reads the analog `input[]` potentiometer states for each color
-// and writes its value on its corresponding PWM `output[]' pin.
-void lightRGB(int input[], int output[]) {
+// Reads the analog `input` potentiometer states for each color
+// and writes its value on its corresponding PWM `output' pin.
+void lightRGB(Channel input, Channel output) {
   // Read potentiometer analog states
-  int red_val = analogRead(input[RED]);
-  int green_val = analogRead(input[GREEN]);
-  int blue_val = analogRead(input[BLUE]);
+  int red_val = analogRead(input.pin_red);
+  int green_val = analogRead(input.pin_green);
+  int blue_val = analogRead(input.pin_blue);
 
   // Since the analog input pins read in 10 bit resolution (0-1023)
   // and PWM output can only be written in 8 bit resolution (0-255),
@@ -46,24 +49,24 @@ void lightRGB(int input[], int output[]) {
   blue_val = round((float)blue_val / 4.0);
 
   // Write the values onto the PWM pins
-  analogWrite(output[RED], red_val);
-  analogWrite(output[GREEN], green_val);
-  analogWrite(output[BLUE], blue_val);
+  analogWrite(output.pin_red, red_val);
+  analogWrite(output.pin_green, green_val);
+  analogWrite(output.pin_blue, blue_val);
 }
 
 void setup() {
   // Set RGB PWM output pins for Channel 1
-  pinMode(OUT_1_PIN[RED], OUTPUT);
-  pinMode(OUT_1_PIN[GREEN], OUTPUT);
-  pinMode(OUT_1_PIN[BLUE], OUTPUT);
+  pinMode(OUT_1.pin_red, OUTPUT);
+  pinMode(OUT_1.pin_green, OUTPUT);
+  pinMode(OUT_1.pin_blue, OUTPUT);
 
   // Set RGB PWM output pins for Channel 2
-  pinMode(OUT_2_PIN[RED], OUTPUT);
-  pinMode(OUT_2_PIN[GREEN], OUTPUT);
-  pinMode(OUT_2_PIN[BLUE], OUTPUT);
+  pinMode(OUT_2.pin_red, OUTPUT);
+  pinMode(OUT_2.pin_green, OUTPUT);
+  pinMode(OUT_2.pin_blue, OUTPUT);
 }
 
 void loop() {
-  lightRGB(IN_1_PIN, OUT_1_PIN);
-  lightRGB(IN_2_PIN, OUT_2_PIN);
+  lightRGB(IN_1, OUT_1);
+  lightRGB(IN_2, OUT_2);
 }
